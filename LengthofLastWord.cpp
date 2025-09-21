856fdd4cafce527c4b0cb9cbce67fbb7a84781dc
Himanshi Patidar <himanshipatidar16@gmail.com>
2025-09-21 13:07:26 +0530

    Initial commit

---
+++ LengthofLastWord.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int lengthOfLastWord(string s)
+{
+  int length = 0;
+  bool foundSpace = false;
+  for (int i = s.length() - 1; i >= 0; i--)
+  {
+    if (s[i] != ' ')
+    {
+      length++;
+      foundSpace = true;
+    }
+    else if (foundSpace)
+    {
+      break;
+    }
+  }
+  return length;
+}
+
+int main()
+{
+  string s;
+  cout << "Enter string: ";
+  getline(cin, s);
+
+  cout << lengthOfLastWord(s);
+
+  return 0;
+}
