d614395128964836e969929a26934de79a4f7bd7
Himanshi Patidar <himanshipatidar16@gmail.com>
2025-09-24 20:28:41 +0530

    Initial commit

---
+++ FindMinimuminRotatedSortedArrayII.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMin(vector<int> &nums)
+{
+  int st = 0, end = nums.size() - 1;
+  while (st < end)
+  {
+    int mid = st + (end - st) / 2;
+    if (nums[mid] > nums[end])
+    {
+      st = mid + 1;
+    }
+    else if (nums[mid] < nums[end])
+    {
+      end = mid;
+    }
+    else
+    {
+      end--;
+    }
+  }
+  return nums[st];
+}
+
+int main()
+{
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+
+  vector<int> nums(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> nums[i];
+  }
+
+  cout << "Minimum is: " << findMin(nums);
+
+  return 0;
+}
