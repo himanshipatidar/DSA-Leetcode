539c1651c90c71b5ad57f7ac5bd85cc1ac5fd7e4
Himanshi Patidar <himanshipatidar16@gmail.com>
2025-09-25 14:32:04 +0530

    Initial commit

---
+++ StringtoInteger.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <climits>
+using namespace std;
+
+int myAtoi(string s)
+{
+  int i = 0, n = s.size();
+  long long result = 0;
+  int sign = 1;
+
+  while (i < n && s[i] == ' ')
+  {
+    i++;
+  }
+
+  if (i < n && (s[i] == '+' || s[i] == '-'))
+  {
+    sign = (s[i] == '+') ? 1 : -1;
+    i++;
+  }
+
+  while (i < n && isdigit(s[i]))
+  {
+    int digit = s[i] - '0';
+    if (result > (INT_MAX - digit) / 10)
+    {
+      return (sign == 1) ? INT_MAX : INT_MIN;
+    }
+    result = result * 10 + digit;
+    i++;
+  }
+
+  return sign * result;
+}
+
+int main()
+{
+  string s;
+  cout << "Enter string: ";
+  cin >> s;
+
+  cout << myAtoi(s);
+
+  return 0;
+}
