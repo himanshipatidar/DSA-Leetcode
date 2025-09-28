2ad24debfc36c210b6fc0d81901801bf12c74a88
Himanshi Patidar <himanshipatidar16@gmail.com>
2025-09-28 10:12:37 +0530

    Initial commit

---
+++ IntegerToRoman.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string intToRoman(int num)
+{
+  int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  string romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+  string result = "";
+  for (int i = 0; i < 13; i++)
+  {
+    while (num >= values[i])
+    {
+      result += romans[i];
+      num = num - values[i];
+    }
+  }
+
+  return result;
+}
+
+int main()
+{
+  int num;
+  cout << "Enter number: ";
+  cin >> num;
+
+  string ans = intToRoman(num);
+  cout << ans;
+}
