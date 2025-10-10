7ca0e68ff5ef828995eef01e25f125344a129088
Himanshi Patidar <himanshipatidar16@gmail.com>
2025-10-10 16:58:28 +0530

    Initial commit

---
+++ OddEvenLinkedList.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class ListNode
+{
+public:
+    int data;
+    ListNode *next;
+
+    ListNode(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    ListNode *head;
+    ListNode *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_back(int val)
+    {
+        ListNode *newNode = new ListNode(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printList()
+    {
+        ListNode *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    ListNode *getHead()
+    {
+        return head;
+    }
+
+    void setHead(ListNode *newHead)
+    {
+        head = newHead;
+    }
+
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenHead = even;
+
+        while (even != NULL && even->next != NULL)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+        return head;
+    }
+};
+
+int main()
+{
+    List list;
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+
+    list.printList();
+
+    list.oddEvenList(list.getHead());
+
+    list.printList();
+
+    return 0;
+}
