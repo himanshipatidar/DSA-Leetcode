2f9b9761b12ce72d756f1159f65198943eb48434
Himanshi Patidar <himanshipatidar16@gmail.com>
2026-01-19 10:49:07 +0530

    added

---
+++ CountOfSubsetsSum.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int countofsubsetsum(int arr[], int n, int sum, int dp[][1000])
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= sum; j++)
+        {
+            if (arr[i - 1] <= j)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - arr[i - 1]];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][sum];
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int sum;
+    cout << "Enter target sum: ";
+    cin >> sum;
+
+    int dp[n + 1][1000];
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < sum + 1; j++)
+        {
+            if (i == 0)
+            {
+                dp[i][j] = 0;
+            }
+            if (j == 0)
+            {
+                dp[i][j] = 1;
+            }
+        }
+    }
+
+    cout << "Count of subsets sum: " << countofsubsetsum(arr, n, sum, dp);
+
+    return 0;
+}
